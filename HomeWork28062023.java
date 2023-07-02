c9d8290adea748a6ae6e7ad2ddec2bab9bb8c7fb
larysamarchuk <133905316+larysamarchuk@users.noreply.github.com>
2023-07-02 16:06:52 +0200

    Add files via upload

---
+++ HomeWork28062023.java
@@ -0,0 +1,14 @@
+package Homework.New;
+
+public class HomeWork28062023 {
+    public static void main(String[] args) {
+        // Инициализация переменной
+        int number = 1;
+
+        // Цикл for для вывода чисел
+        for (int i = 0; i < 10; i++) {
+            System.out.print(number + " ");
+            number *= 2; // Умножаем число на 2 в каждой итерации
+        }
+    }
+}
